7a8b8c7b37cfa3b9491f81157e372a10789e03ca
Roman <rombyr@outlook.com>
2018-11-27 23:10:07 -0500

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,44 @@
+      /* Open this file in your browser and pop open the console to see what your
+      * JavaScript code is doing.  Edit or add to the JavaScript below as you complete
+      * each exercise. */
+
+      // 1. The following function isn't printing anything to the console. Can you spot the syntax error? Be sure to check your console for errors.
+
+      const iLoveCoffee = () => {
+        console.log("Coffee is delicious.");
+      }
+      iLoveCoffee();
+
+
+      // 2. The following function isn't printing anything to the console.  What did we forget?
+
+      const iLovePizza = () =>{
+        console.log("Pizza is my favourite food!");
+      }
+      iLovePizza();
+
+
+      // 3. Write a function `singMeASong()` that logs the text "Hey, I just met you, And this is crazy,But here's my number, So call me, maybe!" to the console.
+      const singMeASong = () =>{
+        console.log("Hey, I just met you, And this is crazy,But here's my number, So call me, maybe!");
+      }
+      singMeASong();
+
+
+      // 4. Change this `iReallyLovePizza` function to accept a single `topping` argument, then log the argument to the console.
+
+      const iReallyLovePizza = (topping) => {
+        console.log("I love pizza!");
+        console.log(`And my favourite topping is ${topping}`);
+        //log your favourite topping to the console here
+      }
+      iReallyLovePizza('chiken');
+
+
+      // 5. Write a function called whatsForDinner that takes one parameter, `(meal)`, which alerts the name of the meal passed to it. Call your function three times with a different meal argument each time.
+      const whatsForDinner = (meal) => {
+        console.log("Broccoli");
+        console.log("Pasta");
+        console.log("Steak");
+      }
+      whatsForDinner();
