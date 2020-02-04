86261bfcf427c6d508875d9eaedf21e8deb04611
Roman <rombyr@outlook.com>
2020-02-04 13:56:14 -0500

    Update script.js

--- day02/script.js
+++ day02/script.js
@@ -1,37 +1,16 @@
-let cookies = 50;
-let biscuits = 24;
 
-if (cookies <= biscuits) {
-    console.log('we have less cookies!')
-} else {
-    console.log('we have more cookies!')
+
+
+function sayMyName(myName) {
+    alert(myName)
 }
 
 
-const numOne = 1;
-const numTwo = 2;
-const numThree = 3;
-let biggestNumber = 0;
 
-if (numOne >= numTwo && numOne >= numThree) {
-    biggestNumber = numOne;
-}
-if (numTwo >= numOne && numTwo >= numThree) {
-    biggestNumber = numTwo;
-}
-if (numThree >= numTwo && numThree >= numOne) {
-    biggestNumber = numThree;
-}
-console.log(biggestNumber);
+sayMyName(Rom);
 
 
-
-
-for (let i = 0; i <= 20; i += 2) {
-    console.log(i);
-}
-
-
-for (let i = 1; i <= 100; i + 2) {
-    console.log(i);
-}
+// const addEm = (a, b, c) => {
+//     console.log(a + b + c);
+// };
+// addEm(1, 2, 3);
