ff10a2c6a619509a8dbd4937fc0a5717e082531c
Roman <rombyr@outlook.com>
2020-02-04 10:06:28 -0500

    Add files via upload

---
+++ day01/script.js
@@ -0,0 +1,13 @@
+alert('run external JS file!');
+
+let seconds = 60;
+let minutes = 60;
+const secondsPerHours = (seconds * minutes) * 24;
+let days = 365;
+const secondsPerYear = secondsPerHours * days;
+console.log(secondsPerYear);
+
+let cookieDough = 12;
+let cookiesAte = (12 * 0.25);
+const cookiesRemaining = cookieDough - cookiesAte;
+console.log(cookiesRemaining);
